ca6c5a59116a6d4f826a7920c3fbc5534228711e
miller2590 <miller2590@gmail.com>
2021-12-06 16:29:22 -0600

    basic functionallity with no styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 class Card extends Component {
   render() {
-    return <div>Hello world</div>;
+    return (
+      <div>
+        <img
+          className="Card"
+          src={this.props.image}
+          alt={this.props.name}
+          id={this.props.id}
+        />
+      </div>
+    );
   }
 }
 
--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -1,17 +1,54 @@
 import React, { Component } from "react";
 import Card from "../Card/Card";
+import axios from "axios";
+
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { deck: null, drawn: [] };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  async componentDidMount() {
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle`);
+
+    this.setState({ deck: deck.data });
+  }
+
+  async handleClick() {
+    let deckID = this.state.deck.deck_id;
+    try {
+      let url = `${API_BASE_URL}/${deckID}/draw/`;
+      let urlData = await axios.get(url);
+      if (!urlData.data.success) {
+        throw new Error("No More Cards!");
+      }
+      let card = urlData.data.cards[0];
+      this.setState((st) => ({
+        drawn: [
+          ...st.drawn,
+          {
+            id: card.code,
+            image: card.image,
+            name: `${card.value} of ${card.suit}`,
+          },
+        ],
+      }));
+    } catch (err) {
+      alert(err);
+    }
   }
 
   render() {
+    const cards = this.state.drawn.map((card) => (
+      <Card name={card.name} image={card.image} id={card.id} />
+    ));
     return (
       <div>
-        <button>Get A Card!</button>
-        <Card />
+        <button onClick={this.handleClick}>Get A Card!</button>
+        {cards}
       </div>
     );
   }
